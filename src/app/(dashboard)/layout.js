daca63cc4870b85e59f3db66f1eeb49569107214
sumitchoudharySEM <sumitsem2004@gmail.com>
2024-05-10 19:24:50 +0530

    created basic dashboard layout

--- src/app/(dashboard)/layout.js
+++ src/app/(dashboard)/layout.js
@@ -1,5 +1,24 @@
 "use client";
+import "./dashboard_layout.css";
 
 export default function UserInfoLayout({ children }) {
-  return <> {children}</>;
+  return <>
+  <div className="layout_container">
+    <div className="layout_header">
+      
+    </div>
+    <div className="layout_base">
+      <div className="layout_sidebar">
+
+      </div>
+      <div className="main_content_container">
+      {children}
+      </div>
+      <div className="layout_right_container">
+
+      </div>
+    </div>
+  </div>
+   
+   </>;
 }
