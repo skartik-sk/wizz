98373dd97777d3c81e13d0592f805a1bc60ac8d9
sumitchoudharySEM <sumitsem2004@gmail.com>
2024-05-10 20:20:52 +0530

    layout @profile page init

--- src/app/(dashboard)/profile/[username]/layout.js
+++ src/app/(dashboard)/profile/[username]/layout.js
@@ -1,5 +1,39 @@
 "use client";
+import Link from "next/link";
+import "./profile_layout.css";
 
 export default function ProfileLayout({ children }) {
-  return<>{ children }</> ;
+  return (
+    <>
+      <div className="profile_container">
+        <div className="main_profile_section">
+          <div className="user_profile_banner_img"></div>
+          <div className="user_info">
+            <h2 className="user_full_name">Sumit Choudhary</h2>
+            <h4 className="user_username">@username</h4>
+            <p className="user_bio">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo,
+              quae.dolor sit amet consectetur adipisicing elit. Nemo,
+            </p>
+            <div className="user_followers">
+              <Link href="/profile/coolsem/followers">
+                <p>Followers: 200</p>
+              </Link>
+              <Link href="/profile/coolsem/following">
+                <p>Following: 200</p>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="profile_menu">
+          <ul>
+            <li>Posts</li>
+            <li>Certificates</li>
+            <li>Achivements</li>
+          </ul>
+        </div>
+        <div className="user_posts_content">{children}</div>
+      </div>
+    </>
+  );
 }
