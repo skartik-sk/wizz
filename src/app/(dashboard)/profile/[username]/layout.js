e1a2e1d02dfb0b7ee11fc84ba395020b7f25f5ca
sumitchoudharySEM <sumitsem2004@gmail.com>
2024-05-10 20:03:17 +0530

    layout at dashboard and navnar work init

--- src/app/(dashboard)/profile/[username]/layout.js
+++ src/app/(dashboard)/profile/[username]/layout.js
@@ -1,5 +1,5 @@
 "use client";
 
 export default function ProfileLayout({ children }) {
-  return { children };
+  return<>{ children }</> ;
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,29 @@
+import Link from "next/link"
+import "./components_css/navbar.css"
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className="main_navbar">
+        <div className="navbar_logo">
+            <h2>Wizz</h2>
+        </div>
+        <div className="navbar_links">
+            <ul>
+                <li><a href="#">Home</a></li>
+                {/* <li><a href="#">Services</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li> */}
+            </ul>
+        </div>
+        <div className="navbar_right">
+            <Link href="/profile/coolsem">
+            <div className="navbar_profile">
+                <h3 className="user_name_navbar">Sumit Choudhary</h3>
+                <p className="user_username_navbar">cool sem</p>
+            </div>
+            </Link>
+        </div>
+    </nav>
   )
 }
 
