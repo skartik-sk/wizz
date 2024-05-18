c51604cae18ee5d168bb6bf416b3f0ff9a377bc0
sumitchoudharySEM <sumitsem2004@gmail.com>
2024-05-18 13:54:44 +0530

    follow unfollow func build

--- src/app/(dashboard)/[username]/followers/page.js
+++ src/app/(dashboard)/[username]/followers/page.js
@@ -4,46 +4,125 @@ import Link from "next/link";
 import { useParams } from "next/navigation";
 import { abi, deployementAddress } from "@/context/wizzAbi.js";
 import { useReadContract } from "wagmi";
+import { BaseError, useWriteContract } from "wagmi";
+import { useAccount } from "wagmi";
 
 const Followers = () => {
+  const { username } = useParams();
+  const { address } = useAccount();
 
-    const { username } = useParams();
+  //user followers array
+  const { data, error, isPending } = useReadContract({
+    abi,
+    address: deployementAddress,
+    functionName: "getUserFollowersByUsername",
+    args: [username],
+  });
 
-    //getting the list of addresses of user followers
-    const {
-        data,
-        error,
-        isPending,
-      } = useReadContract({
-        abi,
-        address: deployementAddress,
-        functionName: "getUserFollowersByUsername",
-        args: [username],
-      });
+  //getting all users
+  const {
+    data: allUsers,
+    isPending: allUsersPanding,
+    error: allUsersError,
+  } = useReadContract({
+    abi,
+    address: deployementAddress,
+    functionName: "getAllUserProfiles",
+    args: [],
+  });
 
-     //getting all users 
-     const{ data:allUsers, isPending: allUsersPanding, error:allUsersError} = useReadContract({
-        abi,
-        address: deployementAddress,
-        functionName: "getAllUserProfiles",
-        args: [],
-      });
+  //select the followers from the list of allUsers according to the address array of followers in data.followers array
+  console.log("data", data);
+  const followers = allUsers?.filter((user) =>
+    data?.includes(user.userAddress)
+  );
 
-    
-      useEffect(() => {
-        console.log(allUsers)
-      }, [allUsers]);
+  //////////////////////////////////////////////////////////////////////////
+
+  const loginUserName = allUsers?.filter((user) => user.userAddress === address);
+  console.log("loginUserName", loginUserName?.[0]?.username);
+
+  // Login User following array
+  const {
+    data: followingData,
+    error: followingError,
+    isPending: followingPanding,
+  } = useReadContract({
+    abi,
+    address: deployementAddress,
+    functionName: "getUserFollowingByUsername",
+    args: [loginUserName?.[0]?.username],
+  });
+
+  //check if the user in followers array is already followed by the login user
+  const following = followers?.filter((user) =>
+    followingData?.includes(user.userAddress)
+  );
+
+  //follow function
+  const {
+    data: followData,
+    error: followError,
+    isPending: followIsPending,
+    writeContract: followWriteContract,
+  } = useWriteContract();
+
+  const follow = async (address) => {
+    console.log("follow function called");
+    followWriteContract({
+      abi,
+      address: deployementAddress,
+      functionName: "follow",
+      args: [address],
+    });
+  };
+
+  //unfollow function
+  const {
+    data: unfollowData,
+    error: unfollowError,
+    isPending: unfollowIsPending,
+    writeContract: unfollowWriteContract,
+  } = useWriteContract();
+
+  const unfollow = async (address) => {
+    console.log("unfollow function called");
+    unfollowWriteContract({
+      abi,
+      address: deployementAddress,
+      functionName: "unfollow",
+      args: [address],
+    });
+  };
+
+  useEffect(() => {
+    console.log("followers", followers);
+    console.log("myfollowingData", followingData);
+  }, [allUsers, followers, followingData]);
 
   return (
     <div>
-      {allUsers?.map((user, i) =>
-      <div className="connection_user">
-        <h1>{allUsers[i].name}</h1>
-        <p>{allUsers[i].username}</p>
-      </div>
-      )}
-    </div>
-  )
-}
+      {followers?.map((user, i) => (
+        <div className="connection_user">
+          <Link href={`/dashboard/${followers[i].username}`}>
+            <h1>{followers[i].name}</h1>
+            <p>{followers[i].username}</p>
+            <p>{followers[i].bio}</p>
+          </Link>
 
-export default Followers
+          {following?.includes(user) ? (
+            <button onClick={() => unfollow(followers[i].userAddress)}>
+              Unfollow
+            </button>
+          ) : (
+            <button onClick={() => follow(followers[i].userAddress)}>
+              Follow
+            </button>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Followers;
