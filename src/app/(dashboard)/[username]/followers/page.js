4d32cc57ea0e69d6216841ade1786a8abda15725
sumitchoudharySEM <sumitsem2004@gmail.com>
2024-05-18 11:32:51 +0530

    FOLLOWERS FOLLOWING SECTIONS LOGIC STARTED BUILDING

--- src/app/(dashboard)/[username]/followers/page.js
+++ src/app/(dashboard)/[username]/followers/page.js
@@ -1,9 +1,47 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+import { abi, deployementAddress } from "@/context/wizzAbi.js";
+import { useReadContract } from "wagmi";
 
 const Followers = () => {
+
+    const { username } = useParams();
+
+    //getting the list of addresses of user followers
+    const {
+        data,
+        error,
+        isPending,
+      } = useReadContract({
+        abi,
+        address: deployementAddress,
+        functionName: "getUserFollowersByUsername",
+        args: [username],
+      });
+
+     //getting all users 
+     const{ data:allUsers, isPending: allUsersPanding, error:allUsersError} = useReadContract({
+        abi,
+        address: deployementAddress,
+        functionName: "getAllUserProfiles",
+        args: [],
+      });
+
+    
+      useEffect(() => {
+        console.log(allUsers)
+      }, [allUsers]);
+
   return (
     <div>
-      Followers
+      {allUsers?.map((user, i) =>
+      <div className="connection_user">
+        <h1>{allUsers[i].name}</h1>
+        <p>{allUsers[i].username}</p>
+      </div>
+      )}
     </div>
   )
 }
